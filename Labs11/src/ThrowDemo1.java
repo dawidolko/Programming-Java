bcc77e04d683f3315ce489c7d2cb7479e62258ed
nullfresh <dawid_olko@outlook.com>
2023-05-25 13:54:35 +0200

    Delete Labs11 directory

--- Labs11/src/ThrowDemo1.java
+++
@@ -1,9 +0,0 @@
-class ThrowsDemo1 {
-    static void metoda() throws IllegalAccessException {
-        System.out.println("Wewnatrz metody");
-        throw new IllegalAccessException("demo");
-    }
-    public static void main(String args[]) throws IllegalAccessException {
-        metoda();
-    }
-}
