db295ef638594e54a4fcf6055f72c754648ce988
nullfresh <dawid_olko@outlook.com>
2023-05-28 22:05:53 +0200

    Add files via upload

---
+++ LAB01/HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.mycompany.helloworld;
+
+import java.util.Date;
+
+/**
+ *
+ * @author dawio
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Witaj, dzisiaj mamy: ");
+        System.out.println(new Date());
+    }
+}
---
+++ LAB01/SystemProperties/src/main/java/com/mycompany/systemproperties/SystemProperties.java
@@ -0,0 +1,18 @@
+package com.mycompany.systemproperties;
+
+/**
+ *
+ * @author dawio
+ */
+public class SystemProperties {
+
+    public static void main(String[] args) {
+        System.out.println("It's all properties:");
+        System.getProperties().list(System.out);
+        System.out.println("\nIt's USER NAME properties:");
+        System.out.println(System.getProperty("user.name"));
+        
+        System.out.println("\nIt's JAVA LIBRARY PATH properties:");
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
