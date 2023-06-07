60e527655a563b0cea301008f66a6a32435f04a6
nullfresh <dawid_olko@outlook.com>
2023-06-07 19:35:32 +0200

    Delete Courses/Kurs 3 directory

--- Courses/Kurs 3/task1/src/Main.java
+++
@@ -1,6 +0,0 @@
-/*Kurs Java od podstaw #1- Wprowadzenie w język Java https://www.youtube.com/watch?v=wa8fZP6IBuU&list=PLTs20Q-BTEMMJHb4GWFT34PAWxYyzndIY*/
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
